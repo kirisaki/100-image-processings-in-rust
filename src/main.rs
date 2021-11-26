7aef1fc47dcf71f16fbe8fbd805ce6112ecfb42e
Akihito KIRISAKI <kirisaki@klaraworks.net>
2021-11-26 14:16:54 +0900

    feat: put pixels

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use image::{flat::Error, io::Reader as ImageReader};
+use image::{GenericImage, Rgba, io::Reader as ImageReader};
 
 fn main() {
-    let img = ImageReader::open("dataset/images/imori_256x256.png").unwrap().decode().unwrap();
+    let mut img = ImageReader::open("dataset/images/imori_256x256.png")
+        .unwrap().decode().unwrap().into_rgba8();
+    for p in img.enumerate_pixels_mut() {
+        *p.2 = Rgba([255, 140, 200, 160]);
+    }
     img.save("test.png");
 }
